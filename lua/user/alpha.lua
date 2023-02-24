19bec6cf2bab0756fdc51a93d0d866ad7909d98a
Simon Zuluaga <simozuluaga@gmail.com>
2023-02-24 18:06:01 -0500

    Funny tron grid ahhaha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,11 @@ end
 
 local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[]],
+  [[]],
+  [[VIM]],
+  [[]],
+  [[]],
 }
 dashboard.section.buttons.val = {
   dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
@@ -22,7 +21,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "The Grid"
 end
 
 dashboard.section.footer.val = footer()
--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,43 +1,19 @@
-local colorscheme = "nightfox"
-local palette = "duskfox"
+local colorscheme = "no-clown-fiesta"
 
--- Default options
 require(colorscheme).setup({
-  options = {
-    -- Compiled file's destination location
-    -- compile_path = vim.fn.stdpath("cache") .. "/nightfox",
-    -- compile_file_suffix = "_compiled", -- Compiled file suffix
-    -- transparent = false,    -- Disable setting background
-    -- terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    -- dim_inactive = false,   -- Non focused panes set to alternative background
-    -- module_default = true,  -- Default enable value for modules
-    styles = {              -- Style to be applied to different syntax groups
-      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
-      constants = "bold",
-      -- conditionals = "NONE",
-      -- functions = "NONE",
-      -- keywords = "NONE",
-      -- numbers = "NONE",
-      -- operators = "NONE",
-      -- strings = "NONE",
-      -- types = "NONE",
-      -- variables = "NONE",
-    },
-    inverse = {             -- Inverse highlight for different types
-      match_paren = false,
-      visual = false,
-      search = false,
-    },
-    -- modules = {             -- List of various plugins and additional options
-    --   -- ...
-    -- },
+  transparent = false, -- Enable this to disable the bg color
+  styles = { 
+    -- You can set any of the style values specified for `:h nvim_set_hl`
+    comments =  {},
+    keywords = {},
+    functions = {},
+    variables = {},
+    type = { bold = true },
+    lsp = { underline = true }
   },
-  palettes = {},
-  specs = {},
-  groups = {},
 })
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. palette)
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
